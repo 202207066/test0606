8aea060c9d3bd04de1b6373050af7903e988c479
mkjung1042 <mkjung1042@gmail.com>
2025-06-08 10:37:56 +0900

    연습용 커밋 [+] mod.cpp

--- calc/mod.cpp
+++ calc/mod.cpp
@@ -1,4 +1,21 @@
-int mod(int __x, int  __y)
-{
-	return 0;
+//
+//  mod.cpp
+//  0513
+//
+//  Created by 정명근 on 5/13/25.
+//
+
+#include "mod.h"
+#include "basic.h"
+int mod(int a, int b) {
+    if (b == 0) return 0; 
+
+    int result = a;
+  
+    while (result >= b) {
+        result = sub(result, b);
+    }
+
+    return result;
 }
+
